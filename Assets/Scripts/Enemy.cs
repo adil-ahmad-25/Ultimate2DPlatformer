9103a4fb81dd518dae9dd82566a61b731653ab77
adil-ahmad-25 <adilahmad.mris@gmail.com>
2024-12-14 23:13:50 +0400

    12

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public Transform player;
+    public float chaseSpeed = 2f;
+    public float jumpForce = 2f;
+    public LayerMask groundLayer;
+
+    private Rigidbody2D rb;
+    private bool isGrounded;
+    private bool shouldJump;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isGrounded = Physics2D.Raycast(transform.position, Vector2.down, 1f, groundLayer);
+
+        float direction = Mathf.Sign(player.position.x - transform.position.x);
+
+        bool isPlayerAbove = Physics2D.Raycast(transform.position, Vector2.up, 3f, 1 << player.gameObject.layer);
+
+        if (isGrounded)
+        {
+            rb.velocity = new Vector2(direction * chaseSpeed, rb.velocity.y);
+        }
+    }
+}
