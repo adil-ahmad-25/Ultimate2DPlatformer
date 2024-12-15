b2613229512b7b44a22b739192a51b032659b19c
adil-ahmad-25 <adilahmad.mris@gmail.com>
2024-12-15 17:15:34 +0400

    end of ep12

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,6 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Numerics;
 using UnityEngine;
+using Vector2 = UnityEngine.Vector2;
+using Vector3 = UnityEngine.Vector3;
 
 public class Enemy : MonoBehaviour
 {
@@ -22,15 +25,50 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //is Grounded
         isGrounded = Physics2D.Raycast(transform.position, Vector2.down, 1f, groundLayer);
 
+        //Player Direction
         float direction = Mathf.Sign(player.position.x - transform.position.x);
 
-        bool isPlayerAbove = Physics2D.Raycast(transform.position, Vector2.up, 3f, 1 << player.gameObject.layer);
+        //Player above detection
+        bool isPlayerAbove = Physics2D.Raycast(transform.position, Vector2.up, 5f, 1 << player.gameObject.layer);
 
         if (isGrounded)
         {
+            //Chase Player
             rb.velocity = new Vector2(direction * chaseSpeed, rb.velocity.y);
+
+            //If Ground
+            RaycastHit2D groundInFront = Physics2D.Raycast(transform.position, new Vector2(direction, 0), 2f, groundLayer);
+
+            //Gap Ahead
+            RaycastHit2D gapAhead = Physics2D.Raycast(transform.position + new Vector3(direction, 0, 0), Vector2.down, 2f, groundLayer);
+
+            //If platform above
+            RaycastHit2D platformAbove = Physics2D.Raycast(transform.position, Vector2.up, 5f, groundLayer);
+
+            if (!groundInFront.collider && !gapAhead.collider)
+            {
+                shouldJump = true;
+            }
+            else if(isPlayerAbove && platformAbove.collider)
+            {
+                shouldJump = true;
+            }
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (isGrounded && shouldJump)
+        {
+            shouldJump = false;
+            Vector2 direction = (player.position - transform.position).normalized;
+
+            Vector2 jumpDirection = direction * jumpForce;
+
+            rb.AddForce(new Vector2(jumpDirection.x, jumpForce), ForceMode2D.Impulse);
         }
     }
 }
