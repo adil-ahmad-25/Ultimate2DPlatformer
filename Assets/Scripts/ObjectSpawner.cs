4299f7fa5c235803cdb36dced1d17110938631ad
adil-ahmad-25 <adilahmad.mris@gmail.com>
2024-12-16 17:11:01 +0400

    ep13

---
+++ Assets/Scripts/ObjectSpawner.cs
@@ -0,0 +1,144 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class ObjectSpawner : MonoBehaviour
+{
+    public enum ObjectType { Gem, Enemy }
+
+    public Tilemap tilemap;
+    public GameObject[] objectPrefabs;
+    public float GemProbability = 0.2f;
+    public float enemyProbability = 0.1f;
+    public int maxObjects = 5;
+    public float gemLifeTime = 10f;
+    public float spawnInterval = 1f;
+
+    private List<Vector3> validSpawnPosition = new List<Vector3>();
+    private List<GameObject> spawnObjects = new List<GameObject>();
+    private bool isSpawning = false;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        GatherValidPositions();
+        StartCoroutine(SpawnObjectsIfNeeded());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private int ActiveObjectCount()
+    {
+        spawnObjects.RemoveAll(item => item = null);
+        return spawnObjects.Count;
+    }
+    
+    private IEnumerator SpawnObjectsIfNeeded()
+    {
+        isSpawning = true;
+        while (ActiveObjectCount() < maxObjects)
+        {
+            SpawnObject();
+            yield return new WaitForSeconds(spawnInterval);
+        }
+        isSpawning = false;
+    }
+
+    private bool PositionHasObject(Vector3 positionToCheck)
+    {
+        return spawnObjects.Any(checkObj => checkObj && Vector3.Distance(checkObj.transform.position, positionToCheck) < 1f);
+    }
+    
+    private ObjectType RandomObjectType()
+    {
+        float randomChoice = Random.value;
+
+        if (randomChoice <= enemyProbability)
+        {
+            return ObjectType.Enemy;
+        }
+        else if(randomChoice <= (enemyProbability + GemProbability))
+        {
+            return ObjectType.Gem;
+        }
+        else
+        {
+            return ObjectType.Gem;
+        }
+    }
+    
+    private void SpawnObject()
+    {
+        if (validSpawnPosition.Count == 0) return;
+
+        Vector3 spawnPosition = Vector3.zero;
+        bool validPositionFound = false;
+
+        while (!validPositionFound && validSpawnPosition.Count > 0)
+        {
+            int randomIndex = Random.Range(0, validSpawnPosition.Count);
+            Vector3 potentialPosition = validSpawnPosition[randomIndex];
+            Vector3 leftPosition = potentialPosition + Vector3.left;
+            Vector3 rightPosition = potentialPosition + Vector3.right;
+
+            if (!PositionHasObject(leftPosition) && !PositionHasObject(rightPosition))
+            {
+                spawnPosition = potentialPosition;
+                validPositionFound = true;
+            }
+
+            validSpawnPosition.RemoveAt(randomIndex);
+        }
+
+        if (validPositionFound)
+        {
+            ObjectType objectType = RandomObjectType();
+            GameObject gameObject = Instantiate(objectPrefabs[(int)objectType], spawnPosition, Quaternion.identity);
+            spawnObjects.Add(gameObject);
+
+            if (objectType != ObjectType.Enemy)
+            {
+                StartCoroutine(DestroyObjectAfterTime(gameObject, gemLifeTime));
+            }
+        }
+    }
+
+    private IEnumerator DestroyObjectAfterTime(GameObject gameObject, float time)
+    {
+        yield return new WaitForSeconds(time);
+
+        if(gameObject)
+        {
+            spawnObjects.Remove(gameObject);
+            validSpawnPosition.Add(gameObject.transform.position);
+            Destroy(gameObject);
+        }
+    }
+
+    private void GatherValidPositions()
+    {
+        validSpawnPosition.Clear();
+        BoundsInt boundsInt = tilemap.cellBounds;
+        TileBase[] allTiles = tilemap.GetTilesBlock(boundsInt);
+        Vector3 start = tilemap.CellToWorld(new Vector3Int(boundsInt.xMin, boundsInt.yMin, 0));
+
+        for (int x = 0; x < boundsInt.size.x; x++)
+        {
+            for (int y = 0; y < boundsInt.size.y; y++)
+            {
+                TileBase tile = allTiles[x + y * boundsInt.size.x];
+                if (tile != null)
+                {
+                    Vector3 place = start + new Vector3(x + 0.5f, y + 2f, 0);
+                    validSpawnPosition.Add(place);
+                }
+            }
+        }
+    }
+}
